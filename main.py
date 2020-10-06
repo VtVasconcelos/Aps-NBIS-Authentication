ecbef42fe8efb2340cb0bf67efa677c9060a9f82
vtVasconcelos <vts.vasconcelos@gmail.com>
2020-10-06 01:05:32 +0000

    init rep

---
+++ main.py
@@ -0,0 +1,139 @@
+import pymysql
+from app import app
+from banco import mysql
+from flask import jsonify
+from flask import flash, request
+
+
+@app.route('/add', methods=['POST'])
+def add_pessoa():
+    try:
+        #aqui eu pego o json
+        _json = request.json
+        #pego uma parte do json que no caso é o nome
+        _name = _json['nome']
+        #se nome existe e se a req é POST
+        if(_name and request.method == 'POST'):
+            #eu guardo o comando que o banco de dados vai receber
+            sqlQuery = 'insert into pessoa(nome,digital,nivel_acesso) values(%s,"nenhuma porra","1")'
+            #eu guardo os valores do json que eu peguei
+            bindData = (_name)
+            #crio uma conexao com o banco
+            conn = mysql.connect()
+            #guardo essa conexao em uma variavel
+            cursor = conn.cursor()
+            #executo o comando, passando o comando e os valores
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+
+
+            #respondo se conseguir
+            response = jsonify('Pessoa added')
+            response.status_code = 200
+            return response
+        else:
+            return not_found()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/search')
+def search():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * from pessoa')
+        rows = cursor.fetchall()
+        response = jsonify(rows)
+        response.status_code = 200
+        return response
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/search/<int:id>')
+def searchById(id):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * from pessoa where id=%s', id)
+        rows = cursor.fetchone()
+        response = jsonify(rows)
+        if(rows == None):
+            message = {
+                'status': 404,
+                'message': "Not found"
+            }
+            response = jsonify(message)
+            response.status_code = 404
+            return response
+        response.status_code = 200
+        return response
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/update', methods=['PUT'])
+def update():
+    try:
+        _json = request.json
+        _id = _json['id']
+        _nome = _json['nome']
+        _digital = _json['digital']
+        _nivel_acesso = _json['nivel_acesso']
+
+        if _nome and _digital and _nivel_acesso and _id and request.method == 'PUT':
+            sqlQuery = "UPDATE pessoa SET nome=%s, digital=%s, nivel_acesso=%s WHERE id=%s"
+            bindData = (_nome, _digital, _nivel_acesso, _id,)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+            response = jsonify('Pessoa updated')
+            response.status_code = 200
+            return response
+        else:
+            return not_found()
+    except Exception as e:
+            print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete(id):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM pessoa WHERE id=%s",(id))
+        conn.commit()
+        response = jsonify("Pessoa aniquilada")
+        response.status_code = 200
+        return response
+    except Exception as ex:
+        print(ex)
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': "Not found" + request.url
+    }
+    response = jsonify(message)
+    response.status_code = 404
+    return response
+
+
+app.run()
---
+++ nbis.py
@@ -0,0 +1,34 @@
+import os
+import subprocess, sys
+import re
+import tempfile
+import shutil
+import attr
+import os.path
+
+def identify(fileName):
+    cmd = "./library/bozorth3 -g ./" + fileName + " ./database/*.xyt"
+    result = os.popen(cmd).read()
+
+    for temp in result.splitlines():
+        if(int(temp)>40):
+            return True
+
+    return False
+
+def mindtct_from_image(image_path):
+    image_full_path = os.path.abspath(image_path)
+    tempdir = tempfile.mkdtemp()
+    oroot = os.path.join(tempdir, 'result')
+    callMindtct = "./library/mindtct "+ image_full_path +" "+ oroot
+    try:
+        os.popen(callMindtct).read()
+        result = ""
+        with open(oroot + '.xyt') as fd:
+            result = fd.read()
+        with open("result.xyt","w") as fd:
+            fd.write(result)
+        return "Sucess"
+
+    finally:
+        shutil.rmtree(tempdir)
